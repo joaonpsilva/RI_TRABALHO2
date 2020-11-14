2e20b0ab920d04fb85c159215f753f0341a044d8
joaonpsilva <joao.npsilva@ua.pt>
2020-11-14 19:38:53 +0000

    cosine distance

---
+++ Weighted_Indexer.py
@@ -0,0 +1,106 @@
+from Indexer import Indexer
+from Corpus import CorpusReader
+from Tokenizer1 import Tokenizer1
+from Tokenizer2 import Tokenizer2
+from Posting import Posting
+import time
+import heapq
+import argparse
+import os
+import psutil
+from math import log10, sqrt
+import collections
+
+process = psutil.Process(os.getpid())
+
+class Weighted_Indexer(Indexer):
+
+    def __init__(self, corpusreader, tokenizer, method=None):
+        super().__init__(corpusreader, tokenizer)
+        self.method = method
+
+    def build_idf(self):
+        for term, valList in self.invertedIndex.items():
+            valList[0] = log10(self.docID/valList[0])
+
+    def index(self):
+        super().index()
+        self.build_idf()
+
+    def addTokensToIndex(self, tokens):
+
+        tokens = collections.Counter(tokens).most_common() #[(token, occur)]
+        
+        token_weights = { term : 1 + log10(occur) for term, occur in tokens}
+
+        doc_length = sqrt(sum([ score**2 for score in token_weights.values()]))
+        
+        for token, score in token_weights.items():
+            
+            score /= doc_length
+            posting = Posting(self.docID, score )
+
+            if token not in self.invertedIndex:
+                self.invertedIndex[token] = [1, [posting]]
+            else:
+                self.invertedIndex[token][1].append(posting)
+    
+
+    def cos_Score(self, query):
+        queryTokens = collections.Counter(self.tokenizer.process(query)).most_common() #[(token, occur)]
+
+        for t in queryTokens:   #remove terms that are not indexed
+            if t[0] not in self.invertedIndex:
+                queryTokens.remove(t)
+
+        query_weights = { term : ((1 + log10(occur)) * self.invertedIndex[term][0])
+                for term, occur in queryTokens}
+
+        query_length = sqrt(sum([ score**2 for score in query_weights.values()]))
+        
+        doc_scores = {}
+        for term, termScore in query_weights.items():
+            termScore /= query_length 
+
+            for doc in self.invertedIndex[term][1]:
+                if doc.docID in doc_scores:
+                    doc_scores[doc.docID] += doc.score * termScore
+                else:
+                    doc_scores[doc.docID] = doc.score * termScore
+
+        bestDocs = heapq.nlargest(10, doc_scores.items(), key=lambda item: item[1])
+        return bestDocs
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-tokenizer", type=int, choices=[1,2], required=True, help="tokenizer")
+    parser.add_argument("-f", type=str, default="all_sources_metadata_2020-03-13.csv", help="text")
+    args = parser.parse_args()
+
+    corpusreader = CorpusReader(args.f)
+    if args.tokenizer == 1:
+        tokenizer = Tokenizer1()
+    else:
+        tokenizer = Tokenizer2()
+
+    #CREATE INDEXER
+    indexer = Weighted_Indexer(corpusreader, tokenizer)
+    
+    #GET RESULTS
+    t1 = time.time()
+    indexer.index()
+    t2 = time.time()
+
+    print('seconds: ', t2-t1)
+    print("Total memory used by program: ", process.memory_info().rss)
+    
+    keyList = list(indexer.invertedIndex.keys())
+    print('Vocabulary size: ', len(keyList))
+
+    #QUERY
+
+    query = input("Query: ")
+    print(indexer.cos_Score(query))
+
