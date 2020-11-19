23995e6cee4923ae02298602970aab557fdc6a6f
bernasrodrigues <bernardorodrigues@ua.pt>
2020-11-19 15:36:13 +0000

    Creating and writting to file (ex: 1.3)

--- Indexer.py
+++ Indexer.py
@@ -10,6 +10,7 @@ import pickle
 
 process = psutil.Process(os.getpid())
 
+
 class Indexer():
     def __init__(self, corpus, tokenizer):
         self.corpusreader = corpus
@@ -19,36 +20,37 @@ class Indexer():
         self.docID = 0
         self.idMapFile = "idMapFile.pickle"
 
-        with open(self.idMapFile, 'wb') as f:   #Init or clean file
+        with open(self.idMapFile, 'wb') as f:  # Init or clean file
             pickle.dump({}, f)
-    
+
     def hasEnoughMemory(self):
         return True
-    
+
     def idMapToDisk(self):
         with open(self.idMapFile, 'rb') as f:
             content = pickle.load(f)
-        
+
         content.update(self.idMap)
 
         with open(self.idMapFile, 'wb') as f:
             pickle.dump(content, f)
-        
+
         self.idMap = {}
-    
+
     def addTokensToIndex(self, tokens):
-        for word in tokens:  #Iterate over token of documents
+        for word in tokens:  # Iterate over token of documents
 
             if word not in self.invertedIndex:
                 self.invertedIndex[word] = [1, [self.docID]]
             else:
-                if self.docID != self.invertedIndex[word][1][-1]:  #check if word did not happen previously in same doc.
+                if self.docID != self.invertedIndex[word][1][
+                    -1]:  # check if word did not happen previously in same doc.
                     self.invertedIndex[word][1].append(self.docID)
                     self.invertedIndex[word][0] += 1
 
     def index(self):
 
-        count=0
+        count = 0
         while self.hasEnoughMemory():
 
             data = self.corpusreader.getNextChunk()
@@ -56,29 +58,47 @@ class Indexer():
                 print("Finished")
                 break
 
-            for document in data:   #Iterate over Chunk of documents
+            for document in data:  # Iterate over Chunk of documents
                 doi, title, abstract = document[0], document[1], document[2]
-                self.idMap[self.docID] = doi    #map ordinal id used in index to real id
+                self.idMap[self.docID] = doi  # map ordinal id used in index to real id
 
                 tokens = self.tokenizer.process(title, abstract)
 
                 self.addTokensToIndex(tokens)
-                
-                self.docID+=1
-            
-            count += self.docID #Write id map tp disk
+
+                self.docID += 1
+
+            count += self.docID  # Write id map tp disk
             if count >= 10000:
                 self.idMapToDisk()
-                count=0
-        
-        self.idMapToDisk()  #id map not needed, free memory
+                count = 0
 
+        self.idMapToDisk()  # id map not needed, free memory
 
+    def write_to_file(self, file="Index.txt"):
+        print("Writing to {}".format(file))
+        # Apagar o ficheiro caso ele exista
+        try:
+            os.remove(file)
+        except OSError:
+            pass
+
+        # Abrir o ficheiro em "append" para adicionar linha a linha (em vez de uma string enorme)
+        f = open(file, "a")
+
+        for term, values in self.invertedIndex.items():
+            string = ('{}:{}'.format(term, values[0]))
+            for posting in values[1]:
+                string += (';{}:{}'.format(posting.docID, posting.score))
+            string += "\n"
+            f.write(string)
+
+        print("File {} created".format(file))
 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("-tokenizer", type=int, choices=[1,2], required=True, help="tokenizer")
+    parser.add_argument("-tokenizer", type=int, choices=[1, 2], required=True, help="tokenizer")
     parser.add_argument("-f", type=str, default="all_sources_metadata_2020-03-13.csv", help="text")
     args = parser.parse_args()
 
@@ -88,22 +108,22 @@ if __name__ == "__main__":
     else:
         tokenizer = Tokenizer2()
 
-    #CREATE INDEXER
+    # CREATE INDEXER
     indexer = Indexer(corpusreader, tokenizer)
-    
-    #GET RESULTS
+
+    # GET RESULTS
     t1 = time.time()
     indexer.index()
     t2 = time.time()
 
-    print('seconds: ', t2-t1)
+    print('seconds: ', t2 - t1)
     print("Total memory used by program: ", process.memory_info().rss)
-    
+
     keyList = list(indexer.invertedIndex.keys())
     print('Vocabulary size: ', len(keyList))
 
     lessUsed = heapq.nsmallest(10, indexer.invertedIndex.items(), key=lambda item: (item[0], item[1][0]))
     print("First 10 terms with 1 doc freq: ", [i[0] for i in lessUsed])
-    
+
     mostUsed = heapq.nlargest(10, indexer.invertedIndex.items(), key=lambda item: item[1][0])
     print("Higher doc freq: ", [(i[0], i[1][0]) for i in mostUsed])
--- Weighted_Indexer.py
+++ Weighted_Indexer.py
@@ -13,6 +13,7 @@ import collections
 
 process = psutil.Process(os.getpid())
 
+
 class Weighted_Indexer(Indexer):
 
     def __init__(self, corpusreader, tokenizer, method=None):
@@ -21,46 +22,45 @@ class Weighted_Indexer(Indexer):
 
     def build_idf(self):
         for term, valList in self.invertedIndex.items():
-            valList[0] = log10(self.docID/valList[0])
-
+            valList[0] = log10(self.docID / valList[0])
 
     def index(self):
         super().index()
         self.build_idf()
 
     def addTokensToIndex(self, tokens):
-        tokens = collections.Counter(tokens).most_common() #[(token, occur)]
-        
-        token_weights = { term : 1 + log10(occur) for term, occur in tokens}
+        tokens = collections.Counter(tokens).most_common()  # [(token, occur)]
 
-        doc_length = sqrt(sum([ score**2 for score in token_weights.values()]))
-        
+        token_weights = {term: 1 + log10(occur) for term, occur in tokens}
+
+        doc_length = sqrt(sum([score ** 2 for score in token_weights.values()]))
+
         for token, score in token_weights.items():
-            
+
             score /= doc_length
-            posting = Posting(self.docID, score )
+            posting = Posting(self.docID, score)
 
             if token not in self.invertedIndex:
                 self.invertedIndex[token] = [1, [posting]]
             else:
                 self.invertedIndex[token][1].append(posting)
-                self.invertedIndex[token][0] += 1    
+                self.invertedIndex[token][0] += 1
 
     def cos_Score(self, query):
-        queryTokens = collections.Counter(self.tokenizer.process(query)).most_common() #[(token, occur)]
+        queryTokens = collections.Counter(self.tokenizer.process(query)).most_common()  # [(token, occur)]
 
-        for t in queryTokens:   #remove terms that are not indexed
+        for t in queryTokens:  # remove terms that are not indexed
             if t[0] not in self.invertedIndex:
                 queryTokens.remove(t)
 
-        query_weights = { term : ((1 + log10(occur)) * self.invertedIndex[term][0])
-                for term, occur in queryTokens}
+        query_weights = {term: ((1 + log10(occur)) * self.invertedIndex[term][0])
+                         for term, occur in queryTokens}
 
-        query_length = sqrt(sum([ score**2 for score in query_weights.values()]))
-        
+        query_length = sqrt(sum([score ** 2 for score in query_weights.values()]))
+
         doc_scores = {}
         for term, termScore in query_weights.items():
-            termScore /= query_length 
+            termScore /= query_length
 
             for doc in self.invertedIndex[term][1]:
                 if doc.docID in doc_scores:
@@ -75,7 +75,7 @@ class Weighted_Indexer(Indexer):
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("-tokenizer", type=int, choices=[1,2], required=True, help="tokenizer")
+    parser.add_argument("-tokenizer", type=int, choices=[1, 2], required=True, help="tokenizer")
     parser.add_argument("-f", type=str, default="all_sources_metadata_2020-03-13.csv", help="text")
     args = parser.parse_args()
 
@@ -85,22 +85,22 @@ if __name__ == "__main__":
     else:
         tokenizer = Tokenizer2()
 
-    #CREATE INDEXER
+    # CREATE INDEXER
     indexer = Weighted_Indexer(corpusreader, tokenizer)
-    
-    #GET RESULTS
+
+    # GET RESULTS
     t1 = time.time()
     indexer.index()
     t2 = time.time()
 
-    print('seconds: ', t2-t1)
+    print('seconds: ', t2 - t1)
     print("Total memory used by program: ", process.memory_info().rss)
-    
+
     keyList = list(indexer.invertedIndex.keys())
     print('Vocabulary size: ', len(keyList))
 
-    #QUERY
-
+    # QUERY
     query = input("Query: ")
     print(indexer.cos_Score(query))
 
+    indexer.write_to_file()
